9939a055c8ad3b03c8e88520e5ceee818bdff3e0
thomaschoooo <thomaschoooo@gmail.com>
2021-06-16 12:17:17 +0800

    fixed the float problem

--- src/grid_path_planner/PathPlanner.py
+++ src/grid_path_planner/PathPlanner.py
@@ -7,20 +7,20 @@ import time
 
 class PathPlanner:
     def __init__(self, end_x, end_y, node_distance, x=1, y=1):
-        self.min_x = x
-        self.min_y = y
+        self.min_x = int(x)
+        self.min_y = int(y)
         self.max_x = 5
         self.max_y = 7
         self.beacon_pos=[(0.38,0.01),(3.84,0.01),(1.48,7.59),(2.54,7.59)]
         self.obstacle = [(2,3)]
-        self.end_x = end_x
-        self.end_y = end_y
+        self.end_x = int(end_x)
+        self.end_y = int(end_y)
         self.node_distance = node_distance
         self.currentPoint = (x, y)
         self.path=[]
         self.route=[]
-        self.num_nodes_x = math.floor(end_x - x)/node_distance
-        self.num_nodes_y = math.floor(end_y - y)/node_distance
+        self.num_nodes_x = int((end_x - x)/node_distance) +1
+        self.num_nodes_y = int((end_y - y)/node_distance) +1
        
         
 
@@ -30,9 +30,9 @@ class PathPlanner:
         # start = time.time()
         final_path=[]
         j=0
-        while j <= self.num_nodes_y:
+        while j < self.num_nodes_y:
             i=0
-            while i <= self.num_nodes_x:
+            while i < self.num_nodes_x:
                 if j%2==0:
                     self.path.append((self.min_x + i * self.node_distance, self.min_y + j * self.node_distance))
                     i+=1
@@ -60,12 +60,20 @@ class PathPlanner:
 
     #Obtain the list of tuple to bring the rover on the shortest path
     def shortestPath(self, currentPoint, targetPoint): #functionName(inputVariableName : InputType) -> returnType:
-        
-        grid = GridWithWeights(self.end_x+1, self.end_y+1)
-        grid.walls=self.obstacle
+
+        currentPoint =  ( (currentPoint[0] - self.min_x)/self.node_distance, (currentPoint[1]-self.min_y)/self.node_distance )
+        targetPoint =  ( (targetPoint[0] - self.min_x)/self.node_distance, (targetPoint[1]-self.min_y)/self.node_distance )
+        grid = GridWithWeights(self.num_nodes_x, self.num_nodes_y)
+        wall = []
+        for g in self.obstacle:
+            wall.append(((g[0] - self.min_x)/self.node_distance,(g[1]-self.min_y)/self.node_distance) )
+        grid.walls = wall
         came_from_bfs = breadth_first_search(grid, currentPoint, targetPoint)
-        
-        return reconstruct_path(came_from_bfs, currentPoint, targetPoint)
+        x = reconstruct_path(came_from_bfs, currentPoint, targetPoint)
+        y=[]
+        for i in x:
+            y.append((i[0] * self.node_distance + self.min_x, i[1] * self.node_distance + self.min_y))
+        return y
 
     def update(self,targetPoint): #working on this now
         self.route = self.shortestPath(self.currentPoint,targetPoint) + self.state(self.currentPoint)[1]
@@ -79,12 +87,12 @@ class PathPlanner:
 
 
 #User Guide
-pp=PathPlanner(end_x=4, end_y=4, node_distance=1) #initiate class
+pp=PathPlanner(end_x=3, end_y=3, node_distance = 0.25) #initiate class
 pp.sweepingPath() #Create grid pathing route for rover (List of tuples)
-pp.currentPoint= ((1.2, 3.8)) #Update current Position
-pp.state(pp.currentPoint)  #Get the state of completion 
+# pp.currentPoint= ((2, 2)) #Update current Position
+# pp.state(pp.currentPoint)  #Get the state of completion 
 print(pp.path)
-print(pp.nearestPoint(pp.currentPoint))
+# print(pp.nearestPoint(pp.currentPoint))
 
 
 
